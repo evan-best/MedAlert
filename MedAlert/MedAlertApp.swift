4756fdadb6703f625dab40c2eaa87efaffb24a34
evan-best <117332327+evan-best@users.noreply.github.com>
2024-05-18 19:34:34 -0230

    Initial Commit

---
+++ MedAlert/MedAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedAlertApp.swift
+//  MedAlert
+//
+//  Created by Evan Best on 2024-05-18.
+//
+
+import SwiftUI
+
+@main
+struct MedAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
